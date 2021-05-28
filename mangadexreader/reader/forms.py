4587e41ae09b7166464d4f321eb6680963c6a578
Yi-Jiahe <47210363+Yi-Jiahe@users.noreply.github.com>
2021-05-28 22:59:36 +0800

    Removed authors and artists from AdvancedSearch form
    Removed until I can get it working; using them in the search requires making requests to find uuids for matching authors and artists

--- mangadexreader/reader/forms.py
+++ mangadexreader/reader/forms.py
@@ -7,7 +7,7 @@ class TitleSearch(forms.Form):
 
 class AdvancedSearch(forms.Form):
     title = forms.CharField(label='Title', required=False)
-    authors = forms.CharField(label='Authors', required=False)
-    artists = forms.CharField(label='Artists', required=False)
+    # authors = forms.CharField(label='Authors', required=False)
+    # artists = forms.CharField(label='Artists', required=False)
     year = forms.IntegerField(label='Year of release', required=False)
     includedTags = forms.MultipleChoiceField(label='Tags', choices=[(tags[tag], tag) for tag in tags.keys()], required=False)
